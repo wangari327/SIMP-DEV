75dcd9ca388985e866d4cb6b6a7222643a04d016
wangari327 <fredrickthuo327@gmail.com>
2022-05-30 20:58:07 +0300

    Update instadl.py

--- tortoolkit/functions/instadl.py
+++ tortoolkit/functions/instadl.py
@@ -17,7 +17,7 @@ torlog = logging.getLogger(__name__)
 
 # some helpers
 def get_caption(post: Post) -> str:
-    """https://instagram.com/hey_insta_003"""
+    """adds link to profile for tagged users"""
     caption = post.caption
     replace = '<a href="https://instagram.com/{}/">{}</a>'
     for mention in post.caption_mentions:
@@ -163,7 +163,21 @@ async def _insta_post_downloader(message):
     )
     if get_val("INSTA_UNAME") is not None and get_val("INSTA_PASS") is not None:
         login = True
-   
+    else:
+        login = False
+
+    if login:
+        try: 
+            insta.login(get_val("INSTA_UNAME"), get_val("INSTA_PASS"))
+            torlog.info("InstaDL running in Logged Mode")
+        except (
+            BadCredentialsException,
+            TwoFactorAuthRequiredException,
+            ConnectionException,
+            TooManyRequestsException,
+        ) as e:
+            await message.edit(f"**InstaDL ERROR:** " + str(e))
+            torlog.warning(str(e))
     else:
         torlog.info("Insta-DL running without credentials")
         await message.edit(
